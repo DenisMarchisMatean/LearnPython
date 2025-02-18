7d5f024997de3bf0cc11dd40f352faec7307a24b
DenisMarchisMatean <marchiss.denis@gmail.com>
2025-02-18 10:34:35 +0200

    Add tests Work in Progress

--- tests/TestFlaskWeatherAPI/test_app.py
+++ tests/TestFlaskWeatherAPI/test_app.py
@@ -1,4 +1,12 @@
 import pytest
+from FlaskWeatherAPI import app
 
-def test_addition():
-    assert 1 + 1 == 2
+
+def test_app_no_query_params():
+    response = app.app.test_client().get('/show_days_per_month_per_year')
+    assert response.status_code == 400
+
+
+def test_app_query_params_to_many_requests():
+    response = app.app.test_client().get('/show_days_per_month_per_year?city=Cluj-Napoca&country_shorten=RO&month=1&year=2025')
+    assert response.status_code == 429
